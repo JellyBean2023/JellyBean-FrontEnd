b8999276aba57200860037f988a142d8d968b077
SuperRookiee <hyunuk0417@gmail.com>
2023-07-11 17:49:07 +0900

    [헤더] 로그인시 아이콘으로 변경

--- src/components/Head/Header.js
+++ src/components/Head/Header.js
@@ -7,6 +7,7 @@ import Image from 'next/image';
 import { HiOutlineViewList } from 'react-icons/hi';
 import { AiOutlineClose } from 'react-icons/ai';
 import { BsBell } from 'react-icons/bs';
+import { FaUserCircle } from "react-icons/fa";
 
 import ChunjaeEdu from '@/assets/img/천재교육.png';
 import ChunjaeTextBook from '@/assets/img/CI/logo.png';
@@ -14,6 +15,7 @@ import EmployGovernment from '@/assets/img/고용노동부.png';
 import GENIA from '@/assets/img/천재교육 디지털 러닝 GENIA.png';
 
 import '@/assets/scss/Head/Header.scss';
+import { signOut, useSession } from "next-auth/react";
 
 const HeaderContainer = styled.header`
     position: fixed;
@@ -37,15 +39,75 @@ const HeaderContainer = styled.header`
     }
 `;
 
+const LoginArea = styled.div`
+    svg {
+        cursor: pointer;
+        font-size: 35px;
+        margin-top: 3px;
+    }
+    
+    & ul.popup {
+        display: none;
+
+        &_show {
+            @media screen and (min-width: 1024px) {
+                position: fixed;
+                background-color: rgb(var(--background-rgb));
+                padding: 10px;
+                border-radius: 5px;
+                border: 1px solid var(--text-color);
+                margin-top: -15px;;
+                margin-left: -30px;
+                z-index: 1;
+
+                .popup_item {
+                    cursor: pointer;
+                    width: 100%;
+                    height: 40px;
+                    line-height: 5px;
+        
+                    &:hover {
+                        color: var(--theme-color);
+                    }
+                }
+            }
+            
+           
+        }
+
+        @media screen and (max-width: 1024px) {
+            display: block;
+            & li:hover {
+                color: black;
+                cursor: pointer;
+            }
+        }
+    }
+
+
+    
+    @media screen and (max-width: 1024px) {
+        li.popup {
+            display: none;
+        }
+    }
+    
+`;
+
 const Header = () => {
     const [navIcon, setNavIcon] = useState(false);
     const [activeScreen, setActiveScreen] = useState(false);
-    const [activeDropdown, setActiveDropdown] = useState(null);
+    const [activeDropdown, setActiveDropdown] = useState(false);
     const navRef = useRef(null);
     const buttonRef = useRef(null);
+    const { status } = useSession();
 
-    const openDropdown = (Dropdown) => {
-        setActiveDropdown(Dropdown);
+    const toggleDropdown = (Dropdown) => {
+        if (activeDropdown === Dropdown) {
+            setActiveDropdown(false);
+        } else {
+            setActiveDropdown(Dropdown);
+        }
     };
 
     useEffect(() => {
@@ -67,7 +129,7 @@ const Header = () => {
     }, []);
 
     useEffect(() => {
-        setNavIcon(prev => !prev);
+        setNavIcon((prev) => !prev);
     }, [activeScreen]);
 
     return (
@@ -100,13 +162,29 @@ const Header = () => {
                             <BsBell className="alert" />
                             <li className="nav_item"><Link href={`/`} className="nav_link">고객센터</Link></li>
                             <li className="nav_item"><Link href={`/`} className="nav_link">공지사항</Link></li>
-                            <li className="nav_item"><Link href={`/login`} className="nav_link">로그인</Link></li>
+                            <LoginArea>
+                                {status === "authenticated" ? (
+                                    <ul>
+                                        <li className="nav_item popup">
+                                            <FaUserCircle onClick={() => toggleDropdown(true)} />
+                                        </li>
+                                        <ul className={activeDropdown ? `popup_show` : `popup`}>
+                                            <li className="nav_item popup_item">
+                                                <Link href={'/'}>마이페이지</Link>
+                                            </li>
+                                            <li className="nav_item popup_item" onClick={() => signOut()} >로그아웃</li>
+                                        </ul>
+                                    </ul>
+                                ) : (
+                                    <li className="nav_item"><Link href={`/login`} className="nav_link">로그인</Link></li>
+                                )}
+                            </LoginArea>
                         </ul>
                     </div>
                 </div>
 
                 <div className="main_header nav_content bd-grid">
-                    <Link href={`/`}><Image className="logo" src={GENIA} alt="Picture" priority/></Link>
+                    <Link href={`/`}><Image className="logo" src={GENIA} alt="Picture" priority /></Link>
                     <div className="nav_menu">
                         <ul className="nav_list">
                             <li className="nav_item dropdown">
