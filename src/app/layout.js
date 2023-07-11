34be99aef3a00dbb263f24d646b46fedc5992475
SuperRookie <hyunuk0417@gmail.com>
2023-07-11 20:29:33 +0900

    폰트 적용: Noto Sans Korean

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,14 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Noto_Sans_KR } from 'next/font/google'
 import Header from "../components/Head/Header"
 import Footer from "../components/Foot/Footer"
 import StyledComponentsRegistry from './lib/registry';
 import Providers from '@/components/Providers';
 
-
-const inter = Inter({ subsets: ['latin'] })
+const notoSansKr = Noto_Sans_KR({
+  subsets: ["latin"],
+  weight: ["100", "400", "700", "900"],
+});
 
 export const metadata = {
   title: '천재교육 디지털 러닝',
@@ -17,7 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={notoSansKr.className}>
         <Providers>
           <Header />
           <StyledComponentsRegistry>
