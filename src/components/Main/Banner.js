1a1394e9b520908d48b99e2fc1f479dd7ce85d49
SuperRookie <hyunuk0417@gmail.com>
2023-07-13 17:37:16 +0900

    [메인] banner 사진불러오기 수정

--- src/components/Main/Banner.js
+++ src/components/Main/Banner.js
@@ -1,7 +1,7 @@
-import React, { memo, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, { Navigation, Pagination, Autoplay } from 'swiper';
+import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
 import 'swiper/scss';
 import 'swiper/scss/navigation';
 import 'swiper/scss/pagination';
@@ -10,15 +10,14 @@ import dummy from '@/assets/img/Dummy.jpg';
 import dummy2 from '@/assets/img/Dummy2.png';
 import banner from '@/assets/scss/Main/Banner.module.scss';
 
-const dummyImg = [dummy, dummy2, dummy, dummy2, dummy];
-
 SwiperCore.use([Navigation, Pagination, Autoplay]);
 
-const Banner = memo(() => {
+const Dummy = [dummy, dummy2,dummy,dummy2,dummy]
+
+const Banner = () => {
   const [fileNames, setFileNames] = useState([]);
   const [imageUrls, setImageUrls] = useState([]);
 
-  // 파일 목록 불러오기
   useEffect(() => {
     const fetchFileNames = async () => {
       try {
@@ -32,7 +31,6 @@ const Banner = memo(() => {
     fetchFileNames();
   }, []);
 
-  // 이미지 불러오기
   useEffect(() => {
     const fetchImageUrl = async (fileName) => {
       try {
@@ -51,8 +49,7 @@ const Banner = memo(() => {
     });
   }, [fileNames]);
 
-  // imageUrls가 없을 경우 dummy 이미지 사용
-  const renderedImages = imageUrls.length > 0 ? imageUrls : dummyImg;
+  const banner = imageUrls > 0 ? imageUrls : Dummy;
 
   return (
     <section className={banner.swiper}>
@@ -65,17 +62,17 @@ const Banner = memo(() => {
         pagination={{ clickable: true }}
         autoplay={{
           delay: 2000,
-          disableOnInteraction: true,
+          disableOnInteraction: true
         }}
       >
-        {renderedImages.map((imgUrl, idx) => (
+        {banner.map((imgUrl, idx) => (
           <SwiperSlide key={idx}>
-            <Image src={imgUrl} alt={dummy} width={100} height={100} />
+            <Image src={imgUrl} alt="" />
           </SwiperSlide>
         ))}
       </Swiper>
     </section>
   );
-});
+};
 
 export default Banner;
