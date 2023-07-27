adc49ab6f03ba8362f46f9a3e46b52b61b7899c0
SuperRookiee <hyunuk0417@gmail.com>
2023-07-27 16:24:57 +0900

    [에디터] 레이아웃 제작

--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -2,46 +2,134 @@ import { styled } from "styled-components";
 import { Editor } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import { Button } from "@mui/material";
+import { useState } from "react";
 const EditorContainer = styled.main`
+    padding: 2rem;
+
+    h1 {
+        b{
+            font-size: 20px;
+        }
+    }
+
     & form {
         display: flex;
-        justify-content: center;
+        flex-direction: column;
+        
+        & > div:nth-child(1) {
+            width: 100%;
+            margin: 20px 0;
 
-        .ProseMirror, .toastui-editor-md-preview p, .ProseMirror.toastui-editor-contents p{
-            color: var(--text-color);
-        }
+            .ProseMirror,
+            .toastui-editor-md-preview p,
+            .ProseMirror.toastui-editor-contents p{
+                color: var(--text-color);
+            }
 
-        .toastui-editor-main.toastui-editor-ww-mode {
-            .ProseMirror {
+            .toastui-editor-main.toastui-editor-ww-mode {
+                .ProseMirror {
+                    background-color: rgb(var(--background-rgb));
+                }
+            }
+
+            .toastui-editor-contents pre,
+            .toastui-editor-md-code-block-line-background{
                 background-color: rgb(var(--background-rgb));
             }
+
+            .toastui-editor.ww-mode {
+                .toastui-editor-contents pre {
+                    border: 1px solid var(--text-color);
+                }
+            }
+
+            .toastui-editor-popup-body {
+                color: black;
+            }
+        }
+
+        #btn {
+            border: 1px solid var(--text-color);
+            width: 15%;
         }
     }
 `
 
 const Edit = (props) => {
     const { id } = props;
-    const placeHolder = {
+    const [formData, setFormData] = useState({
         lecName: "과정 ID",
         cardinalName: "과정 명",
-        lecStatus: "모집여부 (true/false)",
+        lecStatus: "모집 여부 (true/false)",
         lecInfo: "내용"
-      }
-      
+    });
+
+    const handleChange = (value) => {
+        setFormData({
+            ...formData,
+            lecInfo: value
+        });
+    };
+
+    const removeBackticks = (data) => { //백틱제거 함수
+        const lecInfoWithoutBackticks = data.lecInfo.replace(/`/g, '');
+        return {
+            ...data,
+            lecInfo: lecInfoWithoutBackticks
+        };
+    };
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        const editedData = {
+            ID: { id: id },
+            DATA: removeBackticks(formData)
+        };
+        console.log(editedData);
+    };
+
+    const ph_Curriculum = `
+    {
+      lecName: "과정 ID",
+      cardinalName: "과정 명",
+      lecStatus: 모집 여부 (true/false),
+      lecInfo: "내용"
+    }
+    `;
+
+    const ph_Section1 = `
+        {
+            name: "이름",
+            contents: "내용"
+        }
+    `
+
+    //ID에 따른 placeholder 지정
+    let placeholder;
+    if (id === 'CurriculumBox')
+        placeholder = ph_Curriculum;
+    else if (id === 'Section1')
+        placeholder = ph_Section1;
+    else
+        placeholder = "";
+
     return (
         <EditorContainer className="px-24 py-10">
             <h1><b>데이터 수정하기</b> {id}</h1>
-            <form action={``}>
-                <Editor className="editor"  
-                    initialValue={JSON.stringify(placeHolder)}
+            <form onSubmit={submitHandler}>
+                <Editor
+                    className="editorss"
+                    initialValue={placeholder}
                     previewStyle="vertical"
                     height="600px"
                     initialEditType="wysiwyg"
+                    onChange={handleChange}
                 />
+                <Button id="btn" type="submit"> 수정하기 </Button>
             </form>
-            <Button> 수정하기 </Button>
         </EditorContainer>
-    )
-}
+    );
+};
 
-export default Edit;
+export default Edit;  
