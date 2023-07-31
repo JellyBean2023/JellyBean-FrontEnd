5f7ee5df875db7f2c5267bf9ecc47133570bae54
SuperRookiee <hyunuk0417@gmail.com>
2023-07-31 21:30:26 +0900

    [Mypage] 마이페이지 작성

---
+++ src/components/Mypage/MemberInfomation.js
@@ -0,0 +1,261 @@
+'use client';
+import styled from 'styled-components';
+import axios from 'axios';
+import { AiOutlineCheck } from 'react-icons/ai';
+import { useRouter } from 'next/navigation';
+
+
+const MemberInfomationContainer = styled.main`
+ padding: 1rem 10rem;
+`;
+
+const Form = styled.form`
+  width: 50%;
+  transition: margin-left 0.2s ease;
+
+  @media screen and (max-width: 1024px) {
+    padding: 0 50px;
+  }
+
+  p {
+    color: var(--warning-color);
+  }
+`;
+
+const Title = styled.span`
+  position: relative;
+  font-size: 27px;
+  font-weight: 600;
+
+  &::before {
+    content: '';
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    height: 3px;
+    width: 30px;
+    background-color: var(--theme-color);
+  }
+`;
+
+const InputField = styled.div`
+  color: var(--theme-color);
+  position: relative;
+  margin-top: 65px;
+
+  &#type {
+    div {
+      font-size: 20px;
+      margin-bottom: 24px;
+      margin-top: -5px;
+      margin-left: 5px;
+
+      &#join{
+        padding-top: 0px
+      }
+    }
+
+    &input{
+      &user{
+        width: auto;
+      }
+    }
+  }
+
+  input {
+    font-size: 15px;
+    color: var(--theme-color);
+    width: 100%;
+    border: none;
+    border-bottom: solid var(--theme-color) 1px;
+    padding-bottom: 10px;
+    padding-left: 10px;
+    position: relative;
+    background: none;
+    z-index: 5;
+    
+    &#user{
+      width: 20px;
+    }
+
+    &::placeholder {
+      float: right;
+      color: gray;
+    }
+
+    &:focus {
+      outline: none;
+    }
+
+    &#type {
+      &input{
+        width: auto;
+      }
+    }
+  }
+
+  span {
+    display: block;
+    position: absolute;
+    bottom: 0;
+    left: 0%;
+
+    background-color: #666;
+    width: 0;
+    height: 2px;
+    border-radius: 2px;
+    transition: 0.5s;
+  }
+
+  label {
+    position: absolute;
+    color: var(--theme-color);
+    left: 10px;
+    font-size: 20px;
+    bottom: 8px;
+    transition: all .2s;
+  }
+
+  input {
+    &:focus ~ label, &:valid ~ label {
+      font-size: 16px;
+      bottom: 40px;
+      color: #666;
+      font-weight: bold;
+    }
+
+    &:focus ~ span, &:valid ~ span {
+      width: 100%;
+    }
+  }
+  div{
+    padding-bottom: -50px;
+  }
+
+  & #check_icon {
+    display: inline-block;
+    color: green;
+  }
+`;
+
+const SubmitContainer = styled.div`
+  margin: 30px 0 10px 0;
+
+  input {
+    background-color: var(--theme-color);
+    padding: 5px 10px;
+    border: none;
+    border-radius: 15px;
+    color: #fff;
+    font-size: 17px;
+    font-weight: 500;
+    letter-spacing: 1px;
+    cursor: pointer;
+    transition: all 0.3s ease;
+    width: 100%;
+
+    &:hover {
+      background-color: var(--anti-theme-color);
+    }
+  }
+`;
+
+const Text = styled.span`
+  color: var(--accent-link-color);
+  font-size: 14px;
+
+  a.text {
+    color: var(--theme-color);
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+`;
+
+const ApplyButton = styled.button`
+  position: relative;
+  background-color: rgba(78,84,97);
+  border-radius: 10px;
+  padding: 5px 10px;
+  float: right;
+  margin-top: -45px;
+  z-index: 5;
+  color: white;
+  width: auto;
+
+  &:hover {
+    background-color: var(--theme-color);
+  }
+
+  svg {
+    display: inline-block !important;
+    color: green;
+  }
+`;
+
+const UserBox = styled.div`
+  padding-left: 370px;
+  
+  &#right{
+    padding-left: 20px;
+  }
+`;
+
+const Box = styled.div`
+  display: flex;
+`;
+
+
+
+const MemberInfomation = () => {
+
+    const router = useRouter();
+
+    const handleGoToMypage = () => {
+        router.push('/mypage');
+    };
+
+    return (
+        <MemberInfomationContainer>
+            <Title className="title">회원 상세정보</Title>
+
+            {/* value={member.name} */}
+            <InputField><input type="text" />
+                <label>이름</label><span />
+            </InputField>
+
+            {/* value={member.email} */}
+            <InputField><input type="text" />
+                <label>이메일</label><span />
+            </InputField>
+
+            {/* value={member.password} */}
+            <InputField><input type="password" />
+                <label>비밀번호</label><span />
+            </InputField>
+
+            {/* value={member.birth} */}
+            <InputField><input type="text" />
+                <label>생년월일</label><span />
+            </InputField>
+
+            {/* value={member.phone} */}
+            <InputField><input type="text" />
+                <label>휴대전화 번호</label><span />
+            </InputField>
+
+            <InputField id='type'>
+                <Box>
+                    <div>가입유형</div>
+                </Box>
+
+            </InputField>
+            <SubmitContainer><input type="button" onClick={handleGoToMypage} value="마이페이지로 돌아가기" /></SubmitContainer>
+        </MemberInfomationContainer>
+
+    );
+}
+
+export default MemberInfomation;
--- src/components/Mypage/Mypage.js
+++ src/components/Mypage/Mypage.js
@@ -1,6 +1,39 @@
-import Image from 'next/image';
+'use client';
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import { useRecoilValue } from "recoil";
+import { EmailIDState } from "@/components/Login/Login";
+import Link from "next/link";
+
+
+
 
 const Mypage = () => {
+
+  const [members, setMembers] = useState([]);
+  const emailID = useRecoilValue(EmailIDState);
+
+  console.log(members)
+
+  useEffect(() => {
+    const fetchMembers = async () => {
+      try {
+        const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/member/${emailID}`,
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': status
+            },
+          });
+        setMembers(response.data);
+      } catch (error) {
+        console.error('Error fetching members:', error);
+      }
+    };
+    fetchMembers();
+  }, []);
+
+
   return (
     <main className="profile-page">
       <section className="relative block" style={{ height: "500px" }}>
@@ -17,74 +50,38 @@ const Mypage = () => {
         <div className="container mx-auto px-4">
           <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64">
             <div className="px-6">
-              <div className="flex flex-wrap justify-center">
-                <div className="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
-                  <div className="relative">
-                    <div className='bg-black'>
-                      Mypage
-                    </div>
-                  </div>
-                </div>
-                <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
-                  <div className="py-6 px-3 mt-32 sm:mt-0">
-                    <button className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1" type="button" style={{ transition: "all .15s ease" }}>
-                      Connect
-                    </button>
-                  </div>
-                </div>
-                <div className="w-full lg:w-4/12 px-4 lg:order-1">
-                  <div className="flex justify-center py-4 lg:pt-4 pt-8">
-                    <div className="mr-4 p-3 text-center">
-                      <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
-                        22
-                      </span>
-                      <span className="text-sm text-gray-500">Friends</span>
-                    </div>
-                    <div className="mr-4 p-3 text-center">
-                      <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
-                        10
-                      </span>
-                      <span className="text-sm text-gray-500">Photos</span>
-                    </div>
-                    <div className="lg:mr-4 p-3 text-center">
-                      <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
-                        89
-                      </span>
-                      <span className="text-sm text-gray-500">Comments</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
               <div className="text-center mt-12">
                 <h3 className="text-4xl font-semibold leading-normal mb-2 text-gray-800 mb-2">
-                  Jenna Stones
+                  {members.name}
                 </h3>
                 <div className="text-sm leading-normal mt-0 mb-2 text-gray-500 font-bold uppercase">
                   <i className="fas fa-map-marker-alt mr-2 text-lg text-gray-500"></i>{" "}
-                  Los Angeles, California
+                  {members.email}
+                </div>
+                <div className="text-sm leading-normal mt-0 mb-2 text-gray-500 font-bold uppercase">
+                  <i className="fas fa-map-marker-alt mr-2 text-lg text-gray-500"></i>{" "}
+                  tel: {members.phone}
                 </div>
                 <div className="mb-2 text-gray-700 mt-10">
-                  <i className="fas fa-briefcase mr-2 text-lg text-gray-500"></i>
-                  Solution Manager - Creative Tim Officer
-                </div>
-                <div className="mb-2 text-gray-700">
-                  <i className="fas fa-university mr-2 text-lg text-gray-500"></i>
-                  University of Computer Science
+                  <Link href="/memberInfomation" className="font-normal text-pink-500">
+                    회원정보 상세보기
+                  </Link>
                 </div>
               </div>
               <div className="mt-10 py-10 border-t border-gray-300 text-center">
                 <div className="flex flex-wrap justify-center">
-                  <div className="w-full lg:w-9/12 px-4">
-                    <p className="mb-4 text-lg leading-relaxed text-gray-800">
-                      An artist of considerable range, Jenna the name taken by
-                      Melbourne-raised, Brooklyn-based Nick Murphy writes,
-                      performs and records all of his own music, giving it a
-                      warm, intimate feel with a solid groove structure. An
-                      artist of considerable range.
-                    </p>
-                    <a href="#pablo" className="font-normal text-pink-500">
-                      Show more
-                    </a>
+                  <div className="w-full flex flex-col space-y-6 lg:space-y-8 lg:w-9/12 px-4">
+                    <div>
+                      <h3 className="text-4xl font-semibold leading-normal mb-2 text-gray-800 mb-2">
+                        강의신청 현황
+                      </h3>
+                      <div className="text-sm leading-normal mt-0 mb-2 text-gray-500 font-bold uppercase">
+                        <i className="fas fa-map-marker-alt mr-2 text-lg text-blue-500">신청완료</i>
+                      </div>
+                    </div>
+                    <Link href="#pablo" className="font-normal text-pink-500">
+                      상세보기
+                    </Link>
                   </div>
                 </div>
               </div>
