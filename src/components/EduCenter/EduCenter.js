1a8ad72718824d3f9680bfbe48cc3ec337f87a64
kimdonghuyn <say3356@naver.com>
2023-08-08 20:38:27 +0900

    [dev] 교육센터 지도 수정

--- src/components/EduCenter/EduCenter.js
+++ src/components/EduCenter/EduCenter.js
@@ -4,20 +4,19 @@ const EduCenter = () => {
             <div class="absolute inset-0 bg-gray-300">
                 <iframe width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" title="map" scrolling="no" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d470.6639410475112!2d126.88617728832962!3d37.47873405272262!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357b61e1c90472c1%3A0x63e14c960871e0d7!2z66eI66as7Jik7JWE7Jq466CbIDLqtIA!5e0!3m2!1sko!2skr!4v1691490208010!5m2!1sko!2skr"></iframe>
             </div>
-            <div class="container px-5 py-24 mx-auto flex">
-                <div class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md d">
+            <div class="container-xl px-5 py-24 mx-auto flex">
+                <div class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-10 flex flex-col md:ml-auto ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md d">
                     <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">교육대상</h2>
-                    <p class="leading-relaxed mb-5 text-gray-600">천재교육 IT센터</p>
-                    <div class="relative mb-4">
-                        <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
-                        <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
-                    </div>
-                    <div class="relative mb-4">
-                        <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
-                        <textarea id="message" name="message" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
-                    </div>
+                    <p class="leading-relaxed mb-1 text-gray-600">교육시작일로부터 6개월간 월~금요일 9시 ~ 18시(일 8시간)</p>
+                    <p class="leading-relaxed mb-8 text-gray-600">오프라인 교육 참여 가능한 자</p>
+                    <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">교육장소</h2>
+                    <p class="leading-relaxed mb-8 text-gray-600">서울 금천구 디지털로9길 23 마리오아울렛2관 11층 천재IT교육센터</p>
+                    <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">상담문의</h2>
+                    <p class="leading-relaxed mb-1 text-gray-600">이메일 : chanskim@chunjae.co.kr</p>
+                    <p class="leading-relaxed mb-3 text-gray-600">전화: 02-3282-8589</p>
                 </div>
             </div>
+
         </main>
     )
 }
